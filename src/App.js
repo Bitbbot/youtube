074fbf3d35182541621880ce4da21669da6b601e
BitBot <tbmn2017@gmail.com>
2023-01-11 20:41:23 +0300

    refactor: store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
+import { setVideosPerPageAction } from "./store/videosReducer";
 
 function App() {
   const dispatch = useDispatch();
   const videosPerPage = useSelector((state) => state.videosPerPage);
-  return <div className="App">{videosPerPage}</div>;
+  const setVideosPerPage = (number) => {
+    dispatch(setVideosPerPageAction(8));
+  };
+  return (
+    <div className="App">
+      <span>{videosPerPage}</span>
+      <div onClick={() => setVideosPerPage(5)}>plus</div>
+    </div>
+  );
 }
 
 export default App;
--- src/store/videosReducer.js
+++ src/store/videosReducer.js
@@ -4,16 +4,29 @@ const defaultState = {
   videosPerPage: 3,
 };
 
+const SET_VIDEOS = "SET_VIDEOS";
+const SET_CURRENT_ID = "SET_CURRENT_ID";
+const SET_VIDEOS_PER_PAGE = "SET_VIDEOS_PER_PAGE";
 export const videosReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "SET_VIDEOS":
+    case SET_VIDEOS:
       return { ...state, videos: action.payload };
-    case "SET_CURRENT_ID":
+    case SET_CURRENT_ID:
       return { ...state, currentId: action.payload };
-    case "SET_VIDEOS_PER_PAGE":
+    case SET_VIDEOS_PER_PAGE:
       return { ...state, videosPerPage: action.payload };
 
     default:
       return state;
   }
 };
+
+export const setVideosAction = (payload) => ({ type: SET_VIDEOS, payload });
+export const setCurrentIdAction = (payload) => ({
+  type: SET_CURRENT_ID,
+  payload,
+});
+export const setVideosPerPageAction = (payload) => ({
+  type: SET_VIDEOS_PER_PAGE,
+  payload,
+});
