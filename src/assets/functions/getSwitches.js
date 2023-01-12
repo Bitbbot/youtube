bb197f83c0a1bcaee8d3c4d0095afe5f2ce8b67f
BitBot <tbmn2017@gmail.com>
2023-01-12 13:10:29 +0300

    fix: fix logic for switches

--- src/assets/functions/getSwitches.js
+++ src/assets/functions/getSwitches.js
@@ -1,14 +1,14 @@
 export const getSwitches = ({ videos, videosPerPage, currentId }) => {
   const pages = Math.ceil(videos / videosPerPage);
+  const currentPage = Math.ceil((currentId + 1) / videosPerPage);
   let switchesArray = [];
-  const currentPage = currentId / videosPerPage + 1;
   if (currentPage === 1) {
     switchesArray = [
       { isOn: true, switchesOn: currentId },
       { isOn: false, switchesOn: currentId + videosPerPage },
       { isOn: false, switchesOn: currentId + videosPerPage * 2 },
-      // { isOn: false, switchesOn: currentId + videosPerPage * 3 },
-      // { isOn: false, switchesOn: currentId + videosPerPage * 4 },
+      { isOn: false, switchesOn: currentId + videosPerPage * 3 },
+      { isOn: false, switchesOn: currentId + videosPerPage * 4 },
     ];
   } else if (currentPage === 2) {
     switchesArray = [
@@ -16,11 +16,11 @@ export const getSwitches = ({ videos, videosPerPage, currentId }) => {
       { isOn: true, switchesOn: currentId },
       { isOn: false, switchesOn: currentId + videosPerPage },
       { isOn: false, switchesOn: currentId + videosPerPage * 2 },
-      // { isOn: false, switchesOn: currentId + videosPerPage * 3 },
+      { isOn: false, switchesOn: currentId + videosPerPage * 3 },
     ];
   } else if (currentPage === pages - 1) {
     switchesArray = [
-      // { isOn: false, switchesOn: currentId - videosPerPage * 3 },
+      { isOn: false, switchesOn: currentId - videosPerPage * 3 },
       { isOn: false, switchesOn: currentId - videosPerPage * 2 },
       { isOn: false, switchesOn: currentId - videosPerPage },
       { isOn: true, switchesOn: currentId },
@@ -28,8 +28,8 @@ export const getSwitches = ({ videos, videosPerPage, currentId }) => {
     ];
   } else if (currentPage === pages) {
     switchesArray = [
-      // { isOn: false, switchesOn: currentId - videosPerPage * 4 },
-      // { isOn: false, switchesOn: currentId - videosPerPage * 3 },
+      { isOn: false, switchesOn: currentId - videosPerPage * 4 },
+      { isOn: false, switchesOn: currentId - videosPerPage * 3 },
       { isOn: false, switchesOn: currentId - videosPerPage * 2 },
       { isOn: false, switchesOn: currentId - videosPerPage },
       { isOn: true, switchesOn: currentId },
@@ -43,8 +43,5 @@ export const getSwitches = ({ videos, videosPerPage, currentId }) => {
       { isOn: false, switchesOn: currentId + videosPerPage * 2 },
     ];
   }
-  switchesArray = switchesArray.filter((item) => {
-    return item.switchesOn >= 0 && item.switchesOn <= videos - 1;
-  });
   return switchesArray;
 };
