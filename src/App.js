e11f28268306712723bad4447cf9b0ec214f348f
BitBot <tbmn2017@gmail.com>
2023-01-13 14:03:24 +0300

    create: Events component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import "./App.css";
+import Events from "./components/Events/Events";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HomePage />
+      <Events />
     </div>
   );
 }
--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Video from "./Video/Video";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import s from "./Videos.module.css";
-import { setVideosPerPageAction } from "../../store/videosReducer";
-import { useEffect } from "react";
 import indicator from "../../assets/imgs/indicator.gif";
 
 const Videos = () => {
@@ -13,25 +11,8 @@ const Videos = () => {
     currentId,
     currentId + videosPerPage
   );
+
   const isLoading = useSelector((state) => state.isLoading);
-  const dispatch = useDispatch();
-  function handleResize() {
-    const width = window.visualViewport.width;
-    if (width < 767) {
-      dispatch(setVideosPerPageAction(1));
-    } else if (width < 992) {
-      dispatch(setVideosPerPageAction(2));
-    } else {
-      dispatch(setVideosPerPageAction(3));
-    }
-  }
-  useEffect(() => {
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   return (
     <div className={s.wrapper}>
       {isLoading ? (
