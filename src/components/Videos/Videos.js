7dab7a6dab43fa1df5a9196e0bc27161bb94e8fc
BitBot <tbmn2017@gmail.com>
2023-01-15 20:29:58 +0300

    feature: swipe Scroll

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,20 +1,36 @@
 import React from "react";
 import Video from "./Video/Video";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import s from "./Videos.module.css";
+import useSwipe from "../../assets/functions/useSwipe";
 import indicator from "../../assets/imgs/indicator.gif";
+import { setCurrentIdAction } from "../../store/videosReducer";
 
 const Videos = () => {
+  const dispatch = useDispatch();
   const videosPerPage = useSelector((state) => state.videosPerPage);
   const currentId = useSelector((state) => state.currentId);
   const videos = useSelector((state) => state.videos).slice(
     currentId,
     currentId + videosPerPage
   );
+  const videosAll = useSelector((state) => state.videos);
+  const swipeHandlers = useSwipe({
+    onSwipedLeft: () => {
+      if (currentId + videosPerPage < videosAll.length) {
+        dispatch(setCurrentIdAction(currentId + videosPerPage));
+      }
+    },
+    onSwipedRight: () => {
+      if (currentId - videosPerPage >= 0) {
+        dispatch(setCurrentIdAction(currentId - videosPerPage));
+      }
+    },
+  });
 
   const isLoading = useSelector((state) => state.isLoading);
   return (
-    <div className={s.wrapper}>
+    <div className={s.wrapper} id="videos" {...swipeHandlers}>
       {isLoading ? (
         <img src={indicator} alt="NaN" className={s.indicator} />
       ) : (
