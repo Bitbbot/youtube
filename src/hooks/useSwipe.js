efef32407cc69ba447385eb82daef9d1e62ff6d7
BitBot <tbmn2017@gmail.com>
2023-01-18 15:41:19 +0300

    feature: swipe

--- src/hooks/useSwipe.js
+++ src/hooks/useSwipe.js
@@ -22,41 +22,27 @@ export default function useSwipe(videosRef) {
       }
     },
     onBlank: () => {
-      console.log("blank");
       videosRef.current.style = `transform: translateX(${
         (-currentId / videosPerPage) * videosRef.current.offsetWidth + "px"
       }`;
     },
   };
-  console.log("useSwipe");
   const onTouchStart = (e) => {
-    touchEnd.current = 0; // otherwise the swipe is fired even with usual touch events
+    touchEnd.current = 0;
     touchStart.current = e.targetTouches[0].clientX;
-    console.log("START START", e.targetTouches[0].clientX);
-    console.log("START START", touchStart.current);
   };
 
   const onTouchMove = (e) => {
-    console.log(videosRef.current.offsetWidth);
-    if (-touchStart.current + touchEnd.current > 0) {
+    if (currentId !== 0)
       videosRef.current.style = `transform: translateX(${
         (-currentId / videosPerPage) * videosRef.current.offsetWidth -
         touchStart.current +
         touchEnd.current
       }px`;
-    } else
-      videosRef.current.style = `transform: translateX(${
-        (-currentId / videosPerPage) * videosRef.current.offsetWidth -
-        touchStart.current +
-        touchEnd.current
-      }px`;
-
     touchEnd.current = e.targetTouches[0].clientX;
-    console.log("move", touchEnd.current);
   };
 
   const onTouchEnd = () => {
-    console.log("END END", touchEnd.current);
     if (!touchStart.current || !touchEnd.current) return;
     const distance = touchStart.current - touchEnd.current;
     const isLeftSwipe = distance > minSwipeDistance;
